095e5f964b22235d7109c76fe10098c7c2c49148
Ron Paolo Toyhacao <rptoyhacao@gmail.com>
2021-03-13 01:33:18 +0800

    Initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const emailValue = email.value.trim();
+	
+	if(emailValue == '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Please provide a valid email address');
+	} else {
+		setSuccessFor(email, 'Email address sent! We will have you notified');
+	}
+	
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('.emailValidate');
+	formControl.className = 'form-inputContainer error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input, message) {
+	const formControl = input.parentElement;
+    const small = formControl.querySelector('.emailValidate');
+	formControl.className = 'form-inputContainer success';
+    small.innerText = message;
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,3}))$/.test(email);
+}
+
